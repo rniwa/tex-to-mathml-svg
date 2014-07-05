4ffc78948a9097d1131132350b25e4ceef9f2b35
Ryosuke Niwa <ryosuke.niwa@gmail.com>
2014-07-04 19:19:32 -0700

    Add MathJax and a wrapper script to convert Tex into MathML and SVG.

---
+++ tex-to-mathml-svg.js
@@ -0,0 +1,38 @@
+#!/usr/bin/phantomjs
+
+function main() {
+    try {
+        var system = require('system');
+        if (system.args.length < 2) {
+            console.error('Specify LaTeX expression.');
+            phantom.exit(1);
+        }
+        var texExpression = system.args[1];
+
+        var webPage = require('webpage');
+        var page = webPage.create();
+        var results = [];
+        page.open('mathjax-sandbox.html', function (status) {
+            page.evaluate(function (texExpression) {
+                document.body.textContent = '$$' + texExpression + '$$';
+                MathJax.Hub.Queue(function () { window.mathJaxIsDone = true; });
+            }, texExpression);
+            page.onConsoleMessage = function (line) {
+                if (line == 'done')
+                    phantom.exit(0);
+                console.log(line);
+            }
+        });
+
+        setTimeout(function () {
+            console.error("MaxJax couldn't convert the expression in 3s");
+            phantom.exit(2);
+        }, 3000);
+
+    } catch (error) {
+        console.error("Encountered an unexpected error:", error);
+        phantom.exit(-1);
+    }
+}
+
+main();
